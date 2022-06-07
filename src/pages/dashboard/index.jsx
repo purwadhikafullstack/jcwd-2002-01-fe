e48d88b5030b6a1296650fed269e30a45ac25ee9
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-06-07 21:09:33 +0700

    feat: create sidebar admin component

---
+++ src/pages/dashboard/index.jsx
@@ -0,0 +1,11 @@
+import AdminSidebar from "components/AdminSidebar";
+
+const Dashboard = () => {
+  return (
+    <>
+      <AdminSidebar></AdminSidebar>
+    </>
+  );
+};
+
+export default Dashboard;
