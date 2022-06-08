37579c35f800aac5c50c401ad2d80fbc3592f9f2
givennp <98306049+givennp@users.noreply.github.com>
2022-06-08 23:20:44 +0700

    feature: added product card, navbar, and footer component (#5)
    * Squashed commit of the following:
    
    commit 47d85dca9c5155778e14f5292a63410b7bea12b7
    Author: givennp <givennatio2@gmail.com>
    Date:   Mon Jun 6 14:22:19 2022 +0700
    
        test
    
    * feat: added footer, product card, navbar
    
    * feature: added product card, navbar, footer component
    
    * Squashed commit of the following:
    
    commit c8ea9630307ef1f50138fe6cb45572161c92bcf1
    Merge: 04f1fdd f26bba6
    Author: givennp <givennatio2@gmail.com>
    Date:   Wed Jun 8 17:33:53 2022 +0700
    
        Merge branch 'main' of https://github.com/purwadhikafullstack/jcwd-2002-01-fe into purwadhikafullstack-main
    
    commit f26bba679b1282aa927323896acbfa334530388d
    Author: Rickykn <61722793+Rickykn@users.noreply.github.com>
    Date:   Wed Jun 8 15:40:52 2022 +0700
    
        UI: create admin dashboard component analytic,expiredmedicie and activity (#3)
    
        * setup chakra ui
    
        * setup material ui
    
        * feat: create sidebar admin component
    
        * UI: create admin navbar component
    
        * UI: create component activitycard,analytivcard,expiredmedicinecard
    
    commit b3f91fef273a462a137ebe899e53c15bf77a77df
    Author: Rickykn <61722793+Rickykn@users.noreply.github.com>
    Date:   Mon Jun 6 22:09:54 2022 +0700
    
        setup chakra ui (#2)

---
+++ src/components/Footer.jsx
@@ -0,0 +1,133 @@
+import { Box, Grid, Icon, Link, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import logo from "assets/logo.png";
+import { RiWhatsappFill } from "react-icons/ri";
+import { MdEmail, MdPhoneInTalk } from "react-icons/md";
+import { FaFacebookF, FaTwitter} from "react-icons/fa";
+import { AiFillInstagram} from "react-icons/ai"
+
+const Footer = () => {
+  return (
+    <Box sx={{position: "absolute", bottom: "0", width: "100%"}}>
+      <Box padding="60px 96px" sx={{ backgroundColor: "whitesmoke" }}>
+        <Grid container spacing={10}>
+          <Grid item xs={3}>
+            <Image src={logo} width="240px" height="50px" />
+            <Stack spacing="12px" marginTop="20px">
+              <Box display="flex">
+                <Icon sx={{ marginRight: "20px", fontSize: "39px" }}>
+                  {<RiWhatsappFill />}
+                </Icon>
+                <Box>
+                  <Typography fontWeight="700" fontSize="14px">
+                    Chat Whatsapp
+                  </Typography>
+                  <Typography>+62-0212-3802</Typography>
+                </Box>
+              </Box>
+              <Box display="flex">
+                <Icon sx={{ marginRight: "20px", fontSize: "39px" }}>
+                  {<MdEmail />}
+                </Icon>
+                <Box>
+                  <Typography fontWeight="700" fontSize="14px">
+                    Email
+                  </Typography>
+                  <Typography>contact@email.com</Typography>
+                </Box>
+              </Box>
+              <Box display="flex">
+                <Icon sx={{ marginRight: "20px", fontSize: "39px" }}>
+                  {<MdPhoneInTalk />}
+                </Icon>
+                <Box>
+                  <Typography fontWeight="700" fontSize="14px">
+                    Call Center
+                  </Typography>
+                  <Typography>021-123973</Typography>
+                </Box>
+              </Box>
+            </Stack>
+          </Grid>
+          <Grid item xs={3}>
+            <Stack spacing="30px" fontSize="14px">
+              <Link href="#" underline="hover">
+                {"Tentang Kami"}
+              </Link>
+              <Link href="#" underline="hover">
+                {"FAQ"}
+              </Link>
+              <Link href="#" underline="hover">
+                {"Kebijakan Privasi"}
+              </Link>
+              <Link href="#" underline="hover">
+                {"Syarat & Ketentuan"}
+              </Link>
+              <Link href="#" underline="hover">
+                {"Karir"}
+              </Link>
+            </Stack>
+          </Grid>
+          <Grid item xs={3}>
+            <Stack spacing="30px" fontSize="14px">
+              <Link href="#" underline="hover">
+                {"Blog"}
+              </Link>
+              <Link href="#" underline="hover">
+                {"Cara Belanja"}
+              </Link>
+              <Link href="#" underline="hover">
+                {"Promo"}
+              </Link>
+              <Link href="#" underline="hover">
+                {"Diagnosis"}
+              </Link>
+            </Stack>
+          </Grid>
+          <Grid item xs={3}>
+            <Typography
+              fontSize="24px"
+              fontWeight="700"
+              sx={{ marginBottom: "32px" }}
+            >
+              Ikuti Kami
+            </Typography>
+            <Stack spacing="12px">
+              <Box display="flex" alignItems="center">
+                <Icon sx={{ marginRight: "20px", fontSize: "28px" }}>
+                  {<FaFacebookF />}
+                </Icon>
+                <Typography>Facebook</Typography>
+              </Box>
+              <Box display="flex" alignItems="center">
+                <Icon sx={{ marginRight: "20px", fontSize: "28px" }}>
+                  {<FaTwitter />}
+                </Icon>
+                <Typography>Twitter</Typography>
+              </Box>
+              <Box display="flex" alignItems="center">
+                <Icon sx={{ marginRight: "20px", fontSize: "28px" }}>
+                  {<AiFillInstagram />}
+                </Icon>
+                <Typography>Instagram</Typography>
+              </Box>
+            </Stack>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box
+        textAlign="center"
+        sx={{
+          color: "white",
+          backgroundColor: "#4F618E",
+          height: "95px",
+          padding: "36px",
+        }}
+      >
+        designed by ahayde
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
