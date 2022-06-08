321ea93c5a65a3338a1ad39fcea84f78009af24d
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-06-08 15:36:24 +0700

    UI: create component activitycard,analytivcard,expiredmedicinecard

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,12 +1,29 @@
+import { Box, Grid, Typography } from "@mui/material";
+import ActivityCard from "components/ActivityCard";
 import AdminNavbar from "components/AdminNavbar";
 import AdminSidebar from "components/AdminSidebar";
+import AnalyticCard from "components/AnalyticCard";
+import ExpiredMedicineCard from "components/ExpiredMedicineCard";
 
 const Dashboard = () => {
   return (
-    <>
-      <AdminNavbar></AdminNavbar>
-      <AdminSidebar></AdminSidebar>
-    </>
+    <Grid container>
+      <Grid item xs={3} md={2}>
+        <AdminSidebar></AdminSidebar>
+      </Grid>
+      <Grid item xs={9} md={10}>
+        <AdminNavbar></AdminNavbar>
+        <Box>
+          <Box display="flex">
+            <AnalyticCard></AnalyticCard>
+            <AnalyticCard></AnalyticCard>
+            <AnalyticCard></AnalyticCard>
+          </Box>
+
+          <ActivityCard></ActivityCard>
+        </Box>
+      </Grid>
+    </Grid>
   );
 };
 
