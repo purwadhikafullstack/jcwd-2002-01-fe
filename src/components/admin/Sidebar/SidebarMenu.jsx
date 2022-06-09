28cc129ec38a8250905bb0fe447290d9952f6117
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-06-09 17:39:17 +0700

    UI: fixed sidebar admin

--- src/components/admin/Sidebar/SidebarMenu.jsx
+++ src/components/admin/Sidebar/SidebarMenu.jsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
 import { useState } from "react";
 import { Box, Typography, Collapse, MenuItem } from "@mui/material";
-import { ExpandLess, ExpandMore, LocalPharmacy } from "@mui/icons-material";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
 
-const SidebarSubmenu = ({ submenuTitle, href, icon }) => {
+const SidebarSubmenu = ({ submenuTitle, href }) => {
   return (
     <Link href={href}>
       <MenuItem
         sx={{
           color: "#52637A",
+          pl: 4,
           "&:hover": {
-            color: "#009B90",
+            color: "brand.600",
             opacity: [0.9, 0.8, 0.7],
             cursor: "pointer",
           },
@@ -22,18 +23,18 @@ const SidebarSubmenu = ({ submenuTitle, href, icon }) => {
   );
 };
 
-const SidebarMenu = ({ menuTitle, subMenus = [], icon }) => {
+const SidebarMenu = ({ menuTitle, subMenus = [], icon, href = "" }) => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <Box sx={{ mt: 2 }}>
+    <Box sx={{ mt: 3 }}>
       <Box
         display="flex"
         justifyContent="space-between"
         sx={{
           color: "#52637A",
           "&:hover": {
-            color: "#009B90",
+            color: "brand.600",
             opacity: [0.9, 0.8, 0.7],
             cursor: "pointer",
           },
@@ -42,12 +43,17 @@ const SidebarMenu = ({ menuTitle, subMenus = [], icon }) => {
         }}
         onClick={() => setMenuOpen((prevState) => !prevState)}
       >
-        <Box display="flex">
-          {icon}
-          <Typography sx={{ pl: 1 }}>{menuTitle}</Typography>
-        </Box>
-
-        {menuOpen ? <ExpandMore /> : <ExpandLess />}
+        <Link href={href}>
+          <Box display="flex">
+            {icon}
+            <Typography sx={{ pl: 1 }}>{menuTitle}</Typography>
+          </Box>
+        </Link>
+        {subMenus.length ? (
+          <Box>{menuOpen ? <ExpandMore /> : <ExpandLess />}</Box>
+        ) : (
+          <Box></Box>
+        )}
       </Box>
 
       <Collapse in={menuOpen} timeout="auto" unmountOnExit>
