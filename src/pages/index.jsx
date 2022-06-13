03dce51e5fc322409d1336591bfc1256aa924d5c
givennp <givennatio2@gmail.com>
2022-06-14 00:08:28 +0700

    Squashed commit of the following:
    commit 239bf56f77df3267fe0a510c66b2c66988ea8dd4
    Merge: 869d407 1e970fa
    Author: givennp <givennatio2@gmail.com>
    Date:   Tue Jun 14 00:08:01 2022 +0700
    
        Merge branch 'main' of https://github.com/purwadhikafullstack/jcwd-2002-01-fe into purwadhikafullstack-main
    
    commit 1e970fa08724e3f3626262f53bd0b176a94248d1
    Author: Rickykn <61722793+Rickykn@users.noreply.github.com>
    Date:   Thu Jun 9 13:14:11 2022 +0700
    
        setup condition for admin page and customize themen (#6)
    
        * setup chakra ui
    
        * setup material ui
    
        * feat: create sidebar admin component
    
        * UI: create admin navbar component
    
        * UI: create component activitycard,analytivcard,expiredmedicinecard
    
        * UI: setup page for admin container page
    
        * setup customize theme, and fixed admin sidebar UI
    
    commit 37579c35f800aac5c50c401ad2d80fbc3592f9f2
    Author: givennp <98306049+givennp@users.noreply.github.com>
    Date:   Wed Jun 8 23:20:44 2022 +0700
    
        feature: added product card, navbar, and footer component (#5)
    
        * Squashed commit of the following:
    
        commit 47d85dca9c5155778e14f5292a63410b7bea12b7
        Author: givennp <givennatio2@gmail.com>
        Date:   Mon Jun 6 14:22:19 2022 +0700
    
            test
    
        * feat: added footer, product card, navbar
    
        * feature: added product card, navbar, footer component
    
        * Squashed commit of the following:
    
        commit c8ea9630307ef1f50138fe6cb45572161c92bcf1
        Merge: 04f1fdd f26bba6
        Author: givennp <givennatio2@gmail.com>
        Date:   Wed Jun 8 17:33:53 2022 +0700
    
            Merge branch 'main' of https://github.com/purwadhikafullstack/jcwd-2002-01-fe into purwadhikafullstack-main
    
        commit f26bba679b1282aa927323896acbfa334530388d
        Author: Rickykn <61722793+Rickykn@users.noreply.github.com>
        Date:   Wed Jun 8 15:40:52 2022 +0700
    
            UI: create admin dashboard component analytic,expiredmedicie and activity (#3)
    
            * setup chakra ui
    
            * setup material ui
    
            * feat: create sidebar admin component
    
            * UI: create admin navbar component
    
            * UI: create component activitycard,analytivcard,expiredmedicinecard
    
        commit b3f91fef273a462a137ebe899e53c15bf77a77df
        Author: Rickykn <61722793+Rickykn@users.noreply.github.com>
        Date:   Mon Jun 6 22:09:54 2022 +0700
    
            setup chakra ui (#2)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
 import { Box, Typography } from "@mui/material";
-import Example from "components/Carousel";
-
+import Footer from "components/Footer";
 import Navbar from "components/Navbar";
+import ProductCard from "components/ProductCard";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
@@ -10,7 +10,8 @@ import Banner from "../components/Banner";
 const Home = () => {
   return (
     <Box>
-      <Example/>
+      <Navbar />
+      <ProductCard />
     </Box>
   );
 };
