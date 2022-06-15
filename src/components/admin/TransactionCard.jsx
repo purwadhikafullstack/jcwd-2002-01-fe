7ed219fd2828f8123a1b91990892119642b6f37c
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-06-15 14:09:54 +0700

    UI: add pagination in transaction page

--- src/components/admin/TransactionCard.jsx
+++ src/components/admin/TransactionCard.jsx
@@ -14,7 +14,6 @@ import React, { useEffect, useState } from "react";
 import ModalSalinanResep from "components/admin/ModalSalinanResep";
 
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import { borderRadius } from "@mui/system";
 
 const TransactionCard = () => {
   const [salinanResep, setSalinanResep] = useState(false);
