490d436078d7996afad0064b4c891dd32959bb5f
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-06-15 23:13:14 +0700

    UI: create admin login page and add overflow scroll

--- src/pages/admin/transaction/transactions.jsx
+++ src/pages/admin/transaction/transactions.jsx
@@ -1,13 +1,38 @@
-import { Box, Pagination } from "@mui/material";
+import {
+  Box,
+  Container,
+  FormControl,
+  MenuItem,
+  Pagination,
+  Select,
+  Typography,
+} from "@mui/material";
 import TransactionCard from "components/admin/TransactionCard";
 
 const Transaction = () => {
   return (
-    <Box sx={{ p: "20px" }}>
-      <Pagination count={10} />
+    <>
+      <Box sx={{ p: "20px" }}>
+        <Box display="flex" sx={{ alignItems: "center" }}>
+          <Typography sx={{ mr: "5px" }}>Kartu per halaman</Typography>
+          <FormControl>
+            <Select value={2}>
+              <MenuItem value={2}>2</MenuItem>
+              <MenuItem value={5}>5</MenuItem>
+              <MenuItem value={10}>10</MenuItem>
+              <MenuItem value={15}>15</MenuItem>
+            </Select>
+          </FormControl>
+          <Pagination component="div" count={10} />
+        </Box>
+      </Box>
+
       <TransactionCard></TransactionCard>
       <TransactionCard></TransactionCard>
-    </Box>
+      <TransactionCard></TransactionCard>
+      <TransactionCard></TransactionCard>
+      <TransactionCard></TransactionCard>
+    </>
   );
 };
 
