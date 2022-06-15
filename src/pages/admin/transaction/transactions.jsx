36e5bc7fb3254de7877fe6b1d4c686d436e0f30e
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-06-15 14:09:54 +0700

    UI: add pagination in transaction page

--- src/pages/admin/transaction/transactions.jsx
+++ src/pages/admin/transaction/transactions.jsx
@@ -33,7 +33,7 @@ const Transaction = () => {
       <TransactionCard></TransactionCard>
       <TransactionCard></TransactionCard>
       <TransactionCard></TransactionCard>
-    </>
+    </Box>
   );
 };
 
