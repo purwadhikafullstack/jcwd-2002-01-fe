063a09145698f3061b113f9bed31d5832c624533
Nizham Mustaqim <nizhammustaqim@gmail.com>
2022-06-17 17:22:22 +0700

    UI: BannerJaminan Added (#12)

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,11 @@
-import {Paper, Box} from "@mui/material";
+import {Box} from "@mui/material";
 import Image from "next/image"
 import ProgramHamil from "../assets/program-hamil.png"
 import IdulFitri from "assets/idul-fitri.png"
 
 const Banner = () => {
     return (
-        <Box width="90%"  display="flex" alignItems="center" justifyContent="center" >
+        <Box width= "100%"  display="flex" alignItems="center" justifyContent="center" >
             <Box>
             <Image src={ProgramHamil}/> 
             </Box>
