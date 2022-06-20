59b3e0e61e3270da6843fc7b06750c27499a1335
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-06-20 14:23:22 +0700

    UI: create profit ui,setup redux (#16)

--- src/pages/admin/inventory/products.jsx
+++ src/pages/admin/inventory/products.jsx
@@ -1,3 +1,182 @@
-const Products = () => {};
+import { Box, Button, Container } from "@mui/material";
+import TableData from "components/admin/TableData";
+
+const columns = [
+  { field: "id", headerName: "No", width: 70 },
+  { field: "namaObat", headerName: "Nama Obat", width: 130 },
+  { field: "noObat", headerName: "No Obat", width: 130 },
+  {
+    field: "noBpom",
+    headerName: "No.BPOM",
+    width: 130,
+  },
+  {
+    field: "kategori",
+    headerName: "Kategori",
+    sortable: false,
+    width: 130,
+  },
+  { field: "stok", headerName: "Stok", width: 90, type: "number" },
+  { field: "satuan", headerName: "Satuan", width: 130, sortable: false },
+  { field: "nilaiBarang", headerName: "Nilai Barang", width: 130 },
+  { field: "nilaiJual", headerName: "Nilai Jual", width: 130 },
+  {
+    field: "atur",
+    headerName: "Atur",
+    width: 130,
+    renderCell: () => {
+      return <Button variant="outlined">Lihat Detail</Button>;
+    },
+  },
+];
+
+const rows = [
+  {
+    id: 1,
+    namaObat: "Adem Sari",
+    noObat: "A000321",
+    noBpom: "B000521",
+    kategori: "Obat Bebas",
+    stok: 20,
+    satuan: "Box",
+    nilaiBarang: "Rp. 15.000",
+    nilaiJual: "Rp. 44.000",
+  },
+  {
+    id: 2,
+    namaObat: "Adem Sari",
+    noObat: "A000321",
+    noBpom: "B000521",
+    kategori: "Obat Bebas",
+    stok: 10,
+    satuan: "Box",
+    nilaiBarang: "Rp. 15.000",
+    nilaiJual: "Rp. 44.000",
+  },
+  {
+    id: 4,
+    namaObat: "Adem Sari",
+    noObat: "A000321",
+    noBpom: "B000521",
+    kategori: "Obat Bebas",
+    stok: 15,
+    satuan: "Box",
+    nilaiBarang: "Rp. 15.000",
+    nilaiJual: "Rp. 44.000",
+  },
+  {
+    id: 5,
+    namaObat: "Adem Sari",
+    noObat: "A000321",
+    noBpom: "B000521",
+    kategori: "Obat Bebas",
+    stok: 20,
+    satuan: "Box",
+    nilaiBarang: "Rp. 15.000",
+    nilaiJual: "Rp. 44.000",
+  },
+  {
+    id: 6,
+    namaObat: "Adem Sari",
+    noObat: "A000321",
+    noBpom: "B000521",
+    kategori: "Obat Bebas",
+    stok: 10,
+    satuan: "Box",
+    nilaiBarang: "Rp. 15.000",
+    nilaiJual: "Rp. 44.000",
+  },
+  {
+    id: 7,
+    namaObat: "Adem Sari",
+    noObat: "A000321",
+    noBpom: "B000521",
+    kategori: "Obat Bebas",
+    stok: 15,
+    satuan: "Box",
+    nilaiBarang: "Rp. 15.000",
+    nilaiJual: "Rp. 44.000",
+  },
+  {
+    id: 8,
+    namaObat: "Adem Sari",
+    noObat: "A000321",
+    noBpom: "B000521",
+    kategori: "Obat Bebas",
+    stok: 20,
+    satuan: "Box",
+    nilaiBarang: "Rp. 15.000",
+    nilaiJual: "Rp. 44.000",
+  },
+  {
+    id: 9,
+    namaObat: "Adem Sari",
+    noObat: "A000321",
+    noBpom: "B000521",
+    kategori: "Obat Bebas",
+    stok: 10,
+    satuan: "Box",
+    nilaiBarang: "Rp. 15.000",
+    nilaiJual: "Rp. 44.000",
+  },
+  {
+    id: 10,
+    namaObat: "Adem Sari",
+    noObat: "A000321",
+    noBpom: "B000521",
+    kategori: "Obat Bebas",
+    stok: 15,
+    satuan: "Box",
+    nilaiBarang: "Rp. 15.000",
+    nilaiJual: "Rp. 44.000",
+  },
+  {
+    id: 11,
+    namaObat: "Adem Sari",
+    noObat: "A000321",
+    noBpom: "B000521",
+    kategori: "Obat Bebas",
+    stok: 20,
+    satuan: "Box",
+    nilaiBarang: "Rp. 15.000",
+    nilaiJual: "Rp. 44.000",
+  },
+  {
+    id: 12,
+    namaObat: "Adem Sari",
+    noObat: "A000321",
+    noBpom: "B000521",
+    kategori: "Obat Bebas",
+    stok: 10,
+    satuan: "Box",
+    nilaiBarang: "Rp. 15.000",
+    nilaiJual: "Rp. 44.000",
+  },
+  {
+    id: 13,
+    namaObat: "Adem Sari",
+    noObat: "A000321",
+    noBpom: "B000521",
+    kategori: "Obat Bebas",
+    stok: 15,
+    satuan: "Box",
+    nilaiBarang: "Rp. 15.000",
+    nilaiJual: "Rp. 44.000",
+  },
+];
+const Products = () => {
+  return (
+    <Box
+      sx={{
+        p: "20px",
+        height: 400,
+        width: "100%",
+        marginTop: "32px",
+      }}
+    >
+      <TableData columns={columns} rows={rows}></TableData>
+    </Box>
+  );
+};
 
 export default Products;
--- src/pages/admin/transaction/transactions.jsx
+++ src/pages/admin/transaction/transactions.jsx
@@ -1,26 +1,144 @@
 import {
   Box,
+  Button,
+  Checkbox,
   Container,
   FormControl,
+  FormControlLabel,
   MenuItem,
+  OutlinedInput,
   Pagination,
   Select,
+  Stack,
   Typography,
 } from "@mui/material";
 import TransactionCard from "components/admin/TransactionCard";
+import DownloadIcon from "@mui/icons-material/Download";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+import SearchIcon from "@mui/icons-material/Search";
 
 const Transaction = () => {
   return (
-    <>
-      <Box sx={{ p: "20px" }}>
+    <Container sx={{ mt: "24px" }}>
+      {/* Header */}
+      <Box
+        display="flex"
+        sx={{
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Box>
+          <Typography sx={{ fontWeight: "700", fontSize: "20px" }}>
+            Semua Pesanan
+          </Typography>
+        </Box>
+        <Box display="flex">
+          <Button
+            size="small"
+            sx={{ marginRight: "15px" }}
+            variant="outlined"
+            startIcon={<DownloadIcon />}
+          >
+            Unduh PDF
+          </Button>
+          <Button variant="outlined" startIcon={<InsertDriveFileIcon />}>
+            Excel
+          </Button>
+        </Box>
+      </Box>
+
+      {/* Filter */}
+      <Stack direction="row" spacing={2} sx={{ mt: "24px" }}>
+        <Stack item>
+          <OutlinedInput
+            sx={{
+              borderRadius: "10px",
+              width: "328px",
+              height: "42px",
+              backgroundColor: "white",
+              marginRight: "16px",
+            }}
+            placeholder="Cari nama obat"
+            endAdornment={<SearchIcon htmlColor="gray" />}
+          />
+        </Stack>
+        <Stack item>
+          <FormControl sx={{ marginRight: "16px" }}>
+            <Select
+              sx={{
+                borderRadius: "10px",
+                minWidth: "156px",
+                height: "42px",
+                backgroundColor: "white",
+              }}
+              value="obat bebas"
+              displayEmpty
+              autoWidth
+            >
+              <MenuItem disabled value="">
+                Filter
+              </MenuItem>
+              <MenuItem value="Obat Bebas">Obat Bebas</MenuItem>
+              <MenuItem value="Obat Resep">Obat Resep</MenuItem>
+            </Select>
+          </FormControl>
+        </Stack>
+        <Stack item>
+          <FormControl>
+            <Select
+              sx={{
+                borderRadius: "10px",
+                minWidth: "156px",
+                height: "42px",
+                backgroundColor: "white",
+              }}
+              value={2}
+              autoWidth
+              displayEmpty
+            >
+              <MenuItem value="" disabled>
+                Urutkan
+              </MenuItem>
+              <MenuItem value="Terbaru">Terbaru</MenuItem>
+              <MenuItem value="Harga Tertinggi">Harga Tertinggi</MenuItem>
+              <MenuItem value="Harga Terendah">Harga Terendah</MenuItem>
+            </Select>
+          </FormControl>
+        </Stack>
+      </Stack>
+
+      {/* pagination */}
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          mt: "24px",
+          mb: "12px",
+        }}
+      >
+        <Box>
+          <FormControlLabel control={<Checkbox />} label="Pilih Semua" />
+        </Box>
         <Box display="flex" sx={{ alignItems: "center" }}>
           <Typography sx={{ mr: "5px" }}>Kartu per halaman</Typography>
-          <FormControl>
-            <Select value={2}>
+          <FormControl sx={{ marginRight: "30px" }}>
+            <Select
+              sx={{
+                borderRadius: "5px",
+                minWidth: "68px",
+                height: "28px",
+                backgroundColor: "white",
+                borderColor: "brand.500",
+              }}
+              value="2"
+              autoWidth
+              displayEmpty
+            >
               <MenuItem value={2}>2</MenuItem>
+              <MenuItem value={3}>3</MenuItem>
               <MenuItem value={5}>5</MenuItem>
-              <MenuItem value={10}>10</MenuItem>
-              <MenuItem value={15}>15</MenuItem>
             </Select>
           </FormControl>
           <Pagination component="div" count={10} />
@@ -32,7 +150,11 @@ const Transaction = () => {
       <TransactionCard></TransactionCard>
       <TransactionCard></TransactionCard>
       <TransactionCard></TransactionCard>
-    </>
+      <TransactionCard></TransactionCard>
+      <TransactionCard></TransactionCard>
+      <TransactionCard></TransactionCard>
+      <TransactionCard></TransactionCard>
+    </Container>
   );
 };
 
