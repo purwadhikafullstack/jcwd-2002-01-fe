d5aa8aed4f19a139aedf981945c245e9566c1b31
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-06-28 11:38:18 +0700

    feat: login admin done (#21)

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { AuthReducer } from "./reducers/auth";
+import authReducer from "./reducers/auth";
 
 export const store = configureStore({
   reducer: {
-    auth: AuthReducer,
+    auth: authReducer,
   },
 });
+
+export default store;
