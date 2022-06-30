421a3e6cf23abe652f3f7287f95e39c64bbe6275
givennp <givennatio2@gmail.com>
2022-06-30 13:49:37 +0700

    feat/P1-21

--- src/pages/product_list/index.jsx
+++ src/pages/product_list/index.jsx
@@ -43,6 +43,7 @@ const productListPage = () => {
       const res = await axiosInstance.get("/categories");
 
       setCategories(res.data.result);
+      console.log(res.data.result);
     } catch (err) {
       console.log(err);
     }
@@ -254,7 +255,6 @@ const productListPage = () => {
                 onChange={(e, value) => setPage(value)}
               />
             </Box>
-            </Link>
           </Grid>
         </Grid>
       </Box>
