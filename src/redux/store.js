c4cf2f79bb3661698cf5191bb4542f7428f3fc8f
givennp <98306049+givennp@users.noreply.github.com>
2022-07-04 12:16:48 +0700

    [P1-21] : filter by category (#26)
    * feat/P1-21
    
    * ui : cartPage, dropzone, carousel, user filtering sidebar, button component, tab panel, productDetailPage, upload resep
    
    * ui : checkoutcard, created modal template, cart page
    
    * ui: added related products, homepage, productdetail
    
    * feat/P1-21
    
    * feat/P1-21
    
    * feat/P1-21
    
    * feat/P1-21

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./reducers/auth";
-import searchReducer from "./reducers/search";
+import searchReducer from "./reducers/search"
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    search: searchReducer,
+    search: searchReducer
   },
 });
 
