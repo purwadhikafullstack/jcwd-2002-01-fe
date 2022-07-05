acc691d1b4dc96fb4e177a421b0ddafaafcd649c
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-07-05 16:06:32 +0700

    fetch data from data base and create data table

--- src/components/admin/TableData.jsx
+++ src/components/admin/TableData.jsx
@@ -1,23 +1,101 @@
-import { DataGrid } from "@mui/x-data-grid";
-import { useState } from "react";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { Box, Button } from "@mui/material";
 
-const TableData = ({ rows, columns }) => {
-  const [pageSize, setPageSize] = useState(5);
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0,
+  },
+}));
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+// const rows = [
+//   createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+//   createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+//   createData("Eclair", 262, 16.0, 24, 6.0),
+//   createData("Cupcake", 305, 3.7, 67, 4.3),
+//   createData("Gingerbread", 356, 16.0, 49, 3.9),
+// ];
+
+const TableData = ({ rows = [] }) => {
+  console.log(rows);
+
+  const renderTableBody = () => {
+    return rows.map((val) => {
+      return (
+        <TableRow
+          sx={{
+            ":nth-of-type(even)": {
+              backgroundColor: "#D3D3D3",
+            },
+          }}
+        >
+          <TableCell align="center" component="th" scope="row">
+            {val.no}
+          </TableCell>
+          <TableCell align="center">{val.namaObat || "-"}</TableCell>
+          <TableCell align="center">{val.noObat || "-"}</TableCell>
+          <TableCell align="center">{val.noBpom || "-"}</TableCell>
+          <TableCell align="center">{val.kategori || "-"}</TableCell>
+          <TableCell align="center">{val.stok || "-"}</TableCell>
+          <TableCell align="center">{val.satuan || "-"}</TableCell>
+          <TableCell align="center">
+            Rp. {val.nilaiJual.toLocaleString() || "-"}
+          </TableCell>
+          <TableCell align="center">
+            <Button>Edit</Button>
+            <Button>Delete</Button>
+            <Button>Tambah Stock</Button>
+          </TableCell>
+        </TableRow>
+      );
+    });
+  };
   return (
-    <DataGrid
-      rows={rows}
-      columns={columns}
-      onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
-      pageSize={pageSize}
-      rowsPerPageOptions={[5, 10]}
-      rowCount={100}
-      pagination
-      disableSelectionOnClick
-      sx={{
-        boxShadow: 2,
-        borderRadius: "10px",
-      }}
-    />
+    <Box>
+      <TableContainer component={Paper}>
+        <Table aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>No</StyledTableCell>
+              <StyledTableCell align="center">Nama Obat</StyledTableCell>
+              <StyledTableCell align="center">No.Obat</StyledTableCell>
+              <StyledTableCell align="center">No.BPOM</StyledTableCell>
+              <StyledTableCell align="center">Kategori</StyledTableCell>
+              <StyledTableCell align="center">Stok</StyledTableCell>
+              <StyledTableCell align="center">Satuan</StyledTableCell>
+              <StyledTableCell align="center">Nilai Jual</StyledTableCell>
+              <StyledTableCell align="center">Atur</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>{renderTableBody()}</TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 };
 
--- src/pages/admin/inventory/products.jsx
+++ src/pages/admin/inventory/products.jsx
@@ -13,10 +13,13 @@ import SearchIcon from "@mui/icons-material/Search";
 import AddIcon from "@mui/icons-material/Add";
 import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
 import ModalAddProduct from "components/admin/ModalAddProduct";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ModalAddStock from "components/admin/ModalAddStock";
+import axiosInstance from "configs/api";
+import { useRouter } from "next/router";
 
 const Products = () => {
+  const router = useRouter();
   const [Open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => {
@@ -27,6 +30,81 @@ const Products = () => {
   const handleCloseStock = () => {
     setOpenStock(false);
   };
+
+  const [page, setPage] = useState(0);
+  const [rowPerPage, setRowPerPage] = useState(10);
+  const [sortBy, setSortBy] = useState();
+  const [sortDir, setSortDir] = useState();
+  const [filterCategory, setFilterCategory] = useState(
+    router.query.filter_by_category
+  );
+  const [namaObatFilter, setNamaObatFilter] = useState();
+  const [rows, setRows] = useState([]);
+  const [totalData, setTotalData] = useState(0);
+  const [pageIsReady, setPageIsReady] = useState(false);
+
+  const fetchProduct = async () => {
+    try {
+      const res = await axiosInstance.get("/products/quantity", {
+        params: {
+          // // name: searchValue,
+          // selectedCategory: selectedCategory || undefined,
+          // _sortBy: sortBy ? sortBy : undefined,
+          // _sortDir: sortDir ? sortDir : undefined,
+          _limit: 10,
+          _page: 1,
+        },
+      });
+
+      const data = res.data.result.rows;
+
+      setTotalData(res.data.result.count);
+
+      setRows(
+        data.map((val, idx) => {
+          return {
+            no: idx + rowPerPage * page + 1,
+            namaObat: val.name,
+            noObat: val?.no_medicine,
+            noBpom: val?.no_bpom,
+            kategori: val.Category.name,
+            stok: val.Stock_opnames[0]?.amount,
+            satuan: val?.packaging,
+            nilaiJual: val.price,
+            productId: val.id,
+            diskon: val.discount,
+          };
+        })
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  // useEffect(() => {
+  //   if (router.isReady) {
+  //     if (router.query.name) {
+  //       setSearchValue(router.query.name);
+  //     }
+  //     if (router.query._sortDir) {
+  //       setSortDir(router.query._sortDir);
+  //     }
+  //     if (router.query._sortBy) {
+  //       setSortBy(router.query._sortBy);
+  //     }
+  //     if (router.query._page) {
+  //       setPage(parseInt(router.query._page));
+  //     }
+  //     if (router.query.selectedCategory) {
+  //       setSelectedCategory(router.query.selectedCategory);
+  //     }
+  //     setPageIsReady(true);
+  //   }
+  // }, [router.isReady]);
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
   return (
     <Container
       sx={{
@@ -59,6 +137,7 @@ const Products = () => {
           mt: "38px",
           borderRadius: "8px",
           height: "73vh",
+          overflow: "scroll",
         }}
       >
         <Box sx={{ p: 3 }}>
@@ -98,7 +177,9 @@ const Products = () => {
           <Divider />
           <ModalAddProduct open={Open} handleClose={handleClose} />
           <ModalAddStock open={OpenStock} handleClose={handleCloseStock} />
-          {/* <TableData columns={columns} rows={rows}></TableData> */}
+        </Box>
+        <Box sx={{ px: 3, mb: 3 }}>
+          <TableData rows={rows}></TableData>
         </Box>
       </Box>
     </Container>
