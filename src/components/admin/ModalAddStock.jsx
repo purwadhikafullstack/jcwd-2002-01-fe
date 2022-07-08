ed07806895ccc654c29849de7457dd97118386ec
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-07-08 13:42:58 +0700

    [P1-38] [P1-35] fecth product for adim, add product stock admin (#32)
    * fetch data from data base and create data table
    
    * feat: fetch product with pagination and filterby name and add stock product done
    
    * add filter by category
    
    * fix handle submit file
    
    * fix for build web

--- src/components/admin/ModalAddStock.jsx
+++ src/components/admin/ModalAddStock.jsx
@@ -12,57 +12,66 @@ const {
   OutlinedInput,
   FormHelperText,
   Divider,
+  Stepper,
+  Step,
+  StepLabel,
 } = require("@mui/material");
 import CloseIcon from "@mui/icons-material/Close";
 import axiosInstance from "configs/api";
 import { useFormik } from "formik";
+import Image from "next/image";
 import { useEffect, useState } from "react";
+import * as Yup from "yup";
+import BannerTambahProduct from "../../assets/tambah-product.png";
 
-const ModalAddStock = ({ open, handleClose }) => {
-  const [kuantitas, setKuantitas] = useState(0);
+const steps = ["Input New Stock", "Finish"];
+
+const ModalAddStock = ({ open, handleClose, data, fetchProduct }) => {
   const [orderDate, setOrderDate] = useState("");
-  const [productList, setProductList] = useState();
+  const [activeStep, setActiveStep] = useState(0);
+
   const dateHandle = (event) => {
     setOrderDate(event.target.value);
+    formik.setFieldValue("expired_date", event.target.value);
   };
 
-  const increaseAmount = () => {
-    setKuantitas((prev) => prev + 1);
-  };
+  const handleSubmit = async (values) => {
+    try {
+      const newStock = {
+        productName: data.namaObat,
+        quantity: values.quantity,
+        expired_date: values.expired_date,
+        purchasePrice: values.purchasePrice,
+      };
 
-  const decreaseAmount = () => {
-    if (kuantitas < 2) {
-      setKuantitas(1);
+      await axiosInstance.post("/products/addstock", newStock);
+
+      formik.setFieldValue("quantity", 0);
+      formik.setFieldValue("expired_date", "");
+      formik.setFieldValue("purchasePrice", 0);
+
+      fetchProduct();
+      setActiveStep((prevActiveStep) => prevActiveStep + 1);
+    } catch (err) {
+      console.log(err);
     }
-    setKuantitas((prev) => prev - 1);
   };
 
   const formik = useFormik({
     initialValues: {
-      productName: "",
-      quantity: kuantitas,
+      quantity: 0,
       expired_date: orderDate,
       purchasePrice: 0,
     },
+    validationSchema: Yup.object().shape({
+      quantity: Yup.number().min(1),
+      expired_date: Yup.date(),
+      purchasePrice: Yup.number().min(1),
+    }),
     validateOnChange: false,
-    onSubmit: (values) => {
-      console.log(values);
-    },
+    onSubmit: handleSubmit,
   });
 
-  //   const fetchProduct = async () => {
-  //     try {
-  //       const res = await axiosInstance.get("/products/ambilproduct");
-  //       const data = res?.data?.result;
-  //       setProductList(data);
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   };
-
-  //   useEffect(() => {
-  //     fetchProduct();
-  //   }, []);
   return (
     <Modal open={open} onClose={handleClose}>
       <Box
@@ -94,133 +103,179 @@ const ModalAddStock = ({ open, handleClose }) => {
           />
         </Box>
 
-        <Stack spacing={2}>
-          <FormControl sx={{ borderRadius: "4px" }}>
-            <Grid container>
-              <Grid item xs={4}>
-                <FormLabel
-                  sx={{
-                    fontSize: "14px",
-                    color: "black",
-                  }}
-                >
-                  Nama Produk
-                </FormLabel>
-              </Grid>
-              <Grid item xs={8}>
-                <Select
-                  sx={{
-                    backgroundColor: "white",
-                    height: "36px",
-                    minWidth: "226px",
-                  }}
-                  onChange={(e) => {
-                    formik.setFieldValue("productName", e.target.value);
-                  }}
-                  value={formik.values.productName}
-                  autoWidth
-                  //   displayEmpty
-                >
-                  <MenuItem disabled value="Select">
-                    Select
-                  </MenuItem>
-                  {/* {productList?.map((val) => {
-                    return <MenuItem value={val.name}>{val.name}</MenuItem>;
-                  })} */}
-                  <MenuItem value="amoxilin">amoxilin</MenuItem>;
-                </Select>
-              </Grid>
-            </Grid>
-          </FormControl>
-
-          <FormControl sx={{ borderRadius: "4px" }}>
-            <Grid container>
-              <Grid item xs={4}>
-                <FormLabel
-                  sx={{
-                    fontSize: "14px",
-                    color: "black",
-                  }}
-                >
-                  Kuantitas
-                </FormLabel>
-              </Grid>
-              <Grid item xs={8}>
-                <Stack direction="row">
-                  <Box
-                    display="flex"
-                    sx={{ justifyContent: "center", alignItems: "center" }}
-                  >
-                    <Button onClick={decreaseAmount}>-</Button>
-                    <Typography fontSize="16px" marginX="2px">
-                      {kuantitas}
-                    </Typography>
-                    <Button onClick={increaseAmount}>+</Button>
-                  </Box>
-                </Stack>
-              </Grid>
-            </Grid>
-          </FormControl>
-
-          <FormControl sx={{ borderRadius: "4px" }}>
-            <Grid container>
-              <Grid item xs={4}>
-                <FormLabel
-                  sx={{
-                    fontSize: "14px",
-                    color: "black",
-                  }}
-                >
-                  Harga Jual
-                </FormLabel>
-              </Grid>
-              <Grid item xs={8}>
-                <OutlinedInput
-                  onChange={(event) =>
-                    formik.setFieldValue("purchasePrice", event.target.value)
-                  }
-                  sx={{ height: "32px", minWidth: "226px" }}
-                  placeholder="Masukkan Harga Jual"
-                  value={formik.values.purchasePrice}
-                />
-                {/* <FormHelperText error={true}>
-                  {formik.errors.price || " "}
-                </FormHelperText> */}
-              </Grid>
-            </Grid>
-          </FormControl>
-
-          <FormControl sx={{ borderRadius: "4px" }}>
-            <Grid container>
-              <Grid item xs={4}>
-                <FormLabel
-                  sx={{
-                    fontSize: "14px",
-                    color: "black",
-                  }}
-                >
-                  Tanggal Kadaluarsa
-                </FormLabel>
-              </Grid>
-              <Grid item xs={8}>
-                <OutlinedInput
-                  type="date"
-                  sx={{ height: "32px", minWidth: "226px" }}
-                  onChange={dateHandle}
-                  value={orderDate}
-                />
-              </Grid>
-            </Grid>
-          </FormControl>
-        </Stack>
-        <Box sx={{ mt: 3 }}>
-          <Divider orientation="horizontal" />
-          <Box display="flex" justifyContent="end" padding="16px">
-            <Button onClick={formik.handleSubmit} variant="contained">
-              Submit
-            </Button>
-          </Box>
+        <Box>
+          <Stepper activeStep={activeStep} sx={{ mb: 4 }}>
+            {steps.map((label) => {
+              return (
+                <Step key={label}>
+                  <StepLabel>{label}</StepLabel>
+                </Step>
+              );
+            })}
+          </Stepper>
         </Box>
+
+        {activeStep === 1 ? (
+          <>
+            <Box
+              display="flex"
+              sx={{
+                justifyContent: "center",
+                alignItems: "center",
+                flexDirection: "column",
+              }}
+            >
+              <Image src={BannerTambahProduct} />
+              <Typography
+                sx={{
+                  marginTop: "20px",
+                  fontSize: "16px",
+                  color: "Brand.500",
+                  fontWeight: "bold",
+                }}
+              >
+                Stock Berhasil Ditambahkan!
+              </Typography>
+            </Box>
+          </>
+        ) : (
+          <>
+            <Stack spacing={2}>
+              <FormControl sx={{ borderRadius: "4px" }}>
+                <Grid container>
+                  <Grid item xs={4}>
+                    <FormLabel
+                      sx={{
+                        fontSize: "14px",
+                        color: "black",
+                      }}
+                    >
+                      Nama Produk
+                    </FormLabel>
+                  </Grid>
+                  <Grid item xs={8}>
+                    <Typography>{data.namaObat}</Typography>
+                  </Grid>
+                </Grid>
+              </FormControl>
+
+              <FormControl sx={{ borderRadius: "4px" }}>
+                <Grid container>
+                  <Grid item xs={4}>
+                    <FormLabel
+                      sx={{
+                        fontSize: "14px",
+                        color: "black",
+                      }}
+                    >
+                      Current Stock
+                    </FormLabel>
+                  </Grid>
+                  <Grid item xs={8}>
+                    <Typography>{data.stok || 0}</Typography>
+                  </Grid>
+                </Grid>
+              </FormControl>
+
+              <FormControl sx={{ borderRadius: "4px" }}>
+                <Grid container>
+                  <Grid item xs={4}>
+                    <FormLabel
+                      sx={{
+                        fontSize: "14px",
+                        color: "black",
+                      }}
+                    >
+                      Kuantitas
+                    </FormLabel>
+                  </Grid>
+                  <Grid item xs={8}>
+                    <Stack direction="row">
+                      <Box
+                        sx={{ justifyContent: "center", alignItems: "center" }}
+                      >
+                        <OutlinedInput
+                          type="number"
+                          size="small"
+                          placeholder="Masukkan Jumlah Stok"
+                          value={formik.values.quantity}
+                          onChange={(event) =>
+                            formik.setFieldValue("quantity", event.target.value)
+                          }
+                        />
+                        <FormHelperText error={true}>
+                          {formik.errors.quantity || " "}
+                        </FormHelperText>
+                      </Box>
+                    </Stack>
+                  </Grid>
+                </Grid>
+              </FormControl>
+
+              <FormControl sx={{ borderRadius: "4px" }}>
+                <Grid container>
+                  <Grid item xs={4}>
+                    <FormLabel
+                      sx={{
+                        fontSize: "14px",
+                        color: "black",
+                      }}
+                    >
+                      Harga Beli
+                    </FormLabel>
+                  </Grid>
+                  <Grid item xs={8}>
+                    <OutlinedInput
+                      onChange={(event) =>
+                        formik.setFieldValue(
+                          "purchasePrice",
+                          event.target.value
+                        )
+                      }
+                      sx={{ height: "32px", minWidth: "226px" }}
+                      placeholder="Masukkan Harga Jual"
+                      value={formik.values.purchasePrice}
+                    />
+                    <FormHelperText error={true}>
+                      {formik.errors.purchasePrice || " "}
+                    </FormHelperText>
+                  </Grid>
+                </Grid>
+              </FormControl>
+
+              <FormControl sx={{ borderRadius: "4px" }}>
+                <Grid container>
+                  <Grid item xs={4}>
+                    <FormLabel
+                      sx={{
+                        fontSize: "14px",
+                        color: "black",
+                      }}
+                    >
+                      Tanggal Kadaluarsa
+                    </FormLabel>
+                  </Grid>
+                  <Grid item xs={8}>
+                    <OutlinedInput
+                      type="date"
+                      sx={{ height: "32px", minWidth: "226px" }}
+                      onChange={dateHandle}
+                      value={orderDate}
+                    />
+                  </Grid>
+                </Grid>
+              </FormControl>
+            </Stack>
+            <Box sx={{ mt: 3 }}>
+              <Divider orientation="horizontal" />
+              <Box display="flex" justifyContent="end" padding="16px">
+                <Button onClick={formik.handleSubmit} variant="contained">
+                  Submit
+                </Button>
+              </Box>
+            </Box>
+          </>
+        )}
       </Box>
     </Modal>
   );
--- src/pages/product_list/index.jsx
+++ src/pages/product_list/index.jsx
@@ -35,7 +35,6 @@ const productListPage = () => {
   const [searchValue, setSearchValue] = useState(search.searchInput);
   const [selectedCategory, setSelectedCategory] = useState(0);
 
-
   const maxProductPerPage = 4;
 
   const fetchCategory = async () => {
@@ -140,7 +139,7 @@ const productListPage = () => {
           _sortBy: sortBy ? sortBy : undefined,
           _sortDir: sortDir ? sortDir : undefined,
           _page: page ? page : undefined,
-          selectedCategory: selectedCategory || undefined
+          selectedCategory: selectedCategory || undefined,
         },
       });
     }
