65c222b3c11e072baf7ee35543fc0b7e6f740a54
Rickykn <61722793+Rickykn@users.noreply.github.com>
2022-07-08 13:42:58 +0700

    [P1-38] [P1-35] fecth product for adim, add product stock admin (#32)
    * fetch data from data base and create data table
    
    * feat: fetch product with pagination and filterby name and add stock product done
    
    * add filter by category
    
    * fix handle submit file
    
    * fix for build web

--- src/pages/admin/inventory/products.jsx
+++ src/pages/admin/inventory/products.jsx
@@ -3,8 +3,12 @@ import {
   Button,
   Container,
   Divider,
+  FormControl,
   InputAdornment,
+  InputLabel,
+  MenuItem,
   OutlinedInput,
+  Select,
   Typography,
 } from "@mui/material";
 import DownloadIcon from "@mui/icons-material/Download";
@@ -13,20 +17,143 @@ import SearchIcon from "@mui/icons-material/Search";
 import AddIcon from "@mui/icons-material/Add";
 import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
 import ModalAddProduct from "components/admin/ModalAddProduct";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ModalAddStock from "components/admin/ModalAddStock";
+import axiosInstance from "configs/api";
+import { useRouter } from "next/router";
 
 const Products = () => {
+  const router = useRouter();
   const [Open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => {
     setOpen(false);
   };
-  const [OpenStock, setOpenStock] = useState(false);
-  const handleOpenStock = () => setOpenStock(true);
-  const handleCloseStock = () => {
-    setOpenStock(false);
+
+  const [page, setPage] = useState(0);
+  const [rowPerPage, setRowPerPage] = useState(5);
+  const [sortBy, setSortBy] = useState();
+  const [sortDir, setSortDir] = useState();
+  const [filterCategory, setFilterCategory] = useState(
+    router.query.selectedCategory
+  );
+  const [categories, setCategories] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
+
+  const [rows, setRows] = useState([]);
+  const [totalData, setTotalData] = useState(0);
+  const [pageIsReady, setPageIsReady] = useState(false);
+  const [searchValue, setSearchValue] = useState(router.query.product_name);
+
+  const fetchCategory = async () => {
+    try {
+      const res = await axiosInstance.get("/categories");
+
+      setCategories(res.data.result);
+      console.log(res.data.result);
+    } catch (err) {
+      console.log(err);
+    }
   };
+
+  const fetchProduct = async () => {
+    try {
+      const res = await axiosInstance.get("/products/quantity", {
+        params: {
+          name: searchValue,
+          selectedCategory: filterCategory || undefined,
+          _sortBy: sortBy ? sortBy : undefined,
+          _sortDir: sortDir ? sortDir : undefined,
+          _limit: rowPerPage,
+          _page: page + 1,
+        },
+      });
+
+      const data = res.data.result.rows;
+
+      setTotalData(res.data.result.count);
+
+      setRows(
+        data.map((val, idx) => {
+          return {
+            no: idx + rowPerPage * page + 1,
+            namaObat: val.name,
+            noObat: val?.no_medicine,
+            noBpom: val?.no_bpom,
+            kategori: val.Category.name,
+            stok: val.Stock_opnames[0]?.amount,
+            satuan: val?.packaging,
+            nilaiJual: val.price,
+            productId: val.id,
+            diskon: val.discount,
+          };
+        })
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  const searchInputHandler = (event) => {
+    const { value } = event.target;
+
+    setSearchInput(value);
+  };
+
+  const searchButton = () => {
+    setSearchValue(searchInput);
+    setPage(0);
+  };
+
+  useEffect(() => {
+    if (router.isReady) {
+      if (router.query.product_name) {
+        setSearchValue(router.query.product_name);
+      }
+      if (router.query.selectedCategory) {
+        setFilterCategory(router.query.selectedCategory);
+      }
+      setPageIsReady(true);
+    }
+  }, [router.isReady]);
+
+  useEffect(() => {
+    fetchCategory();
+
+    if (pageIsReady) {
+      fetchProduct();
+
+      if (searchValue || filterCategory) {
+        router.push({
+          query: {
+            product_name: searchValue,
+            selectedCategory: filterCategory || undefined,
+          },
+        });
+      }
+      if (searchValue === "" || filterCategory === "") {
+        router.replace("/admin/inventory/products", undefined, {
+          shallow: true,
+        });
+      }
+    }
+  }, [rowPerPage, page, searchValue, pageIsReady, filterCategory]);
+
+  const renderCategory = () => {
+    return categories.map((val) => {
+      return <MenuItem value={val.id}>{val.name}</MenuItem>;
+    });
+  };
+
   return (
     <Container
       sx={{
@@ -59,6 +186,7 @@ const Products = () => {
           mt: "38px",
           borderRadius: "8px",
           height: "73vh",
+          overflow: "scroll",
         }}
       >
         <Box sx={{ p: 3 }}>
@@ -68,24 +196,39 @@ const Products = () => {
             alignItems="center"
             marginBottom="24px"
           >
-            <OutlinedInput
-              placeholder="Cari nama obat"
-              sx={{ width: "328px", height: "42px" }}
-              endAdornment={
-                <InputAdornment>
-                  <SearchIcon />
-                </InputAdornment>
-              }
-            />
+            <Box
+              display="flex"
+              sx={{ justifyContent: "center", alignItems: "center" }}
+            >
+              <OutlinedInput
+                placeholder="Cari nama obat"
+                onChange={searchInputHandler}
+                sx={{ width: "328px", height: "42px" }}
+                endAdornment={
+                  <InputAdornment>
+                    <SearchIcon
+                      onClick={searchButton}
+                      sx={{
+                        ":hover": {
+                          cursor: "pointer",
+                        },
+                      }}
+                    />
+                  </InputAdornment>
+                }
+              />
+              <FormControl sx={{ m: 1, minWidth: 220 }} size="small">
+                <InputLabel>Filter</InputLabel>
+                <Select
+                  label="Filter"
+                  onChange={(e) => setFilterCategory(e.target.value)}
+                >
+                  <MenuItem value="">None</MenuItem>
+                  {renderCategory()}
+                </Select>
+              </FormControl>
+            </Box>
             <Box>
-              <Button
-                variant="contained"
-                startIcon={<AddIcon />}
-                onClick={handleOpenStock}
-                sx={{ mr: 2 }}
-              >
-                Tambah Stock
-              </Button>
               <Button
                 variant="contained"
                 startIcon={<AddIcon />}
@@ -96,9 +239,22 @@ const Products = () => {
             </Box>
           </Box>
           <Divider />
-          <ModalAddProduct open={Open} handleClose={handleClose} />
-          <ModalAddStock open={OpenStock} handleClose={handleCloseStock} />
-          {/* <TableData columns={columns} rows={rows}></TableData> */}
+          <ModalAddProduct
+            open={Open}
+            handleClose={handleClose}
+            fetchProduct={fetchProduct}
+          />
+        </Box>
+        <Box sx={{ px: 3, mb: 3 }}>
+          <TableData
+            rows={rows}
+            page={page}
+            rowPerPage={rowPerPage}
+            handleChangePage={handleChangePage}
+            handleChangeRowsPerPage={handleChangeRowsPerPage}
+            totalData={totalData}
+            fetchProduct={fetchProduct}
+          ></TableData>
         </Box>
       </Box>
     </Container>
