9ab039c2176708803eef29d4eda3a0e68c1093e5
givennp <98306049+givennp@users.noreply.github.com>
2022-07-21 12:46:06 +0700

    Feat/[P1-6] [P1-29] (#41)
    * feat/P1-22
    
    * ui : cartPage, dropzone, carousel, user filtering sidebar, button component, tab panel, productDetailPage, upload resep
    
    * ui : checkoutcard, created modal template, cart page
    
    * ui: added related products, homepage, productdetail
    
    * feat/P1-21
    
    * feat/P1-21
    
    * [P1-3] [P1-14] login user and admin  (#23)
    
    * setup redux for login and keep login
    
    * feat: login admin and user done with protected page admin
    
    * fixed link routes in navbar
    
    * Feat/P1-21: filtering (#24)
    
    * feat/P1-21
    
    * ui : cartPage, dropzone, carousel, user filtering sidebar, button component, tab panel, productDetailPage, upload resep
    
    * ui : checkoutcard, created modal template, cart page
    
    * ui: added related products, homepage, productdetail
    
    * feat/P1-21
    
    * feat/P1-21
    
    * feat/P1-21
    
    * feat: user can see a product detail page (#25)
    
    * feat/P1-29
    
    Co-authored-by: Rickykn <61722793+Rickykn@users.noreply.github.com>
    Co-authored-by: Nizham Mustaqim <nizhammustaqim@gmail.com>

--- src/pages/upload_prescription/index.jsx
+++ src/pages/upload_prescription/index.jsx
@@ -1,16 +1,87 @@
-import { Box, Typography } from "@mui/material";
+import { Alert, Box, Snackbar, Typography } from "@mui/material";
 import Dropzone from "components/Dropzone";
+import Footer from "components/Footer";
+import axiosInstance from "configs/api";
+import { useRouter } from "next/router";
+import { useState } from "react";
 
 const uploadPrescriptionPage = () => {
-  return ( 
-    <Box
-      sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
-    >
-      <Box>
-        <Typography fontSize="24px" fontWeight="700">Kirim Resep</Typography>
-        <Dropzone />
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [alert, setAlert] = useState(false);
+  const [alertContent, setAlertContent] = useState("");
+  const [severity, setSeverity] = useState();
+  const router = useRouter();
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setAlert(false);
+  };
+
+  const onDrop = (acceptedFiles) => {
+    setSelectedFile(acceptedFiles[0]);
+  };
+
+  const uploadPrescription = async () => {
+    const formData = new FormData();
+
+    formData.append("prescription_image_file", selectedFile);
+
+    setTimeout(async () => {
+      try {
+        const res = await axiosInstance.post("/transactions", formData);
+        router.push("/");
+
+        if (res?.data?.message !== undefined) {
+          setAlertContent("upload prescription success");
+          setAlert(true);
+          setSeverity(true);
+        }
+      } catch (err) {
+        setAlertContent(err?.response?.data?.message);
+        setAlert(true);
+        setSeverity(false);
+        console.log(err);
+      }
+    }, 3000);
+  };
+
+  return (
+    <>
+      {alert ? (
+        <Snackbar
+          open={alert}
+          autoHideDuration={6000}
+          onClose={handleClose}
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        >
+          <Alert variant="filled" severity={severity ? "success" : "error"}>
+            {alertContent}
+          </Alert>
+        </Snackbar>
+      ) : (
+        <></>
+      )}
+      <Box
+        sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+      >
+        <Box sx={{ width: "700px" }}>
+          <Typography fontSize="24px" fontWeight="700" sx={{ my: "50px" }}>
+            Kirim Resep
+          </Typography>
+          <Dropzone
+            isResep={true}
+            onDrop={onDrop}
+            prescriptionImage={selectedFile}
+            upload={() => uploadPrescription()}
+            setPrescriptionImage={setSelectedFile}
+          />
+        </Box>
       </Box>
-    </Box>
+      <Footer />
+    </>
   );
 };
 
